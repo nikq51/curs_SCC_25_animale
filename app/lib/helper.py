54ec14f70b60761309a7865c24a382899efdc77b
Crina20 <ioanacrina2002@gmail.com>
2025-05-07 18:25:36 +0300

    Am corectat path-ul din Jenkinsfile si am adaugat lib helper

---
+++ app/lib/helper.py
@@ -0,0 +1,8 @@
+def descriere_pisica():
+    return "Pisica este un animal bland, curios si adorabil."
+
+def culoare_pisica():
+    return "Pisica este de culoare gri cu alb si are ochii verzi."
+
+def lista_nume_pisici():
+    return ["Mitzi", "Gogosica", "MiauMiau", "Smotoc", "Norisor", "Captain Whiskers", "Zgubi"]
