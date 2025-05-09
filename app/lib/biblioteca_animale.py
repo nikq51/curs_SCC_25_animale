2b02f95b9f9b12444c425c12aca99aa64baaf4f7
Steff0602 <stefalidia405@gmail.com>
2025-05-09 14:35:24 +0300

    Create biblioteca_animale.py

---
+++ app/lib/biblioteca_animale.py
@@ -0,0 +1,5 @@
+def culoare_cal();
+   return "Culoarea unui cal poate varia de la alb, negru sau maro, pana la diferite combinatii de culori."
+
+def descriere_cal();
+  return "Calul este mamifer ierbivor domesticit de mii de ani"
