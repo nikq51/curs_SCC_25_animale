42401c2ac91b8af993a0e65723dc8221d1972307
Crina20 <ioanacrina2002@gmail.com>
2025-05-09 19:39:10 +0300

    Am adaugat proiectul Puma

---
+++ app/lib/helper.py
@@ -0,0 +1,14 @@
+def descriere_puma():
+    return "Puma este un mamifer agil, solitar și extrem de adaptabil."
+
+def culoare_puma():
+    return "Are blana cafenie-aurie, perfectă pentru camuflaj."
+
+def locatii_puma():
+    return [
+        "Munții Anzi",
+        "Munții Stâncoși",
+        "America Centrală",
+        "Deșertul Arizona",
+        "Patagonia"
+    ]
