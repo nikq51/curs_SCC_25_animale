bfd21f3a7927a2fdb36d8666fcbf2cf21f8f4c5e
MeHighh <mmanea387@gmail.com>
2025-05-10 17:56:04 +0000

    conf Jenkins si teste

---
+++ app/tests/test_biblioteca_animale.py
@@ -0,0 +1,23 @@
+from app.lib import biblioteca_animale as b_animale
+
+def test_culoare_arici():
+	cul = b_animale.culoare_arici()
+	if cul == 'gri':
+		assert True
+	else:
+		assert False
+
+def test_hrana_arici():
+	inf = b_animale.hrana_arici()
+	if inf == 'insecte':
+		assert True
+	else:
+		assert False
+
+def test_invelisul_corpului_arici():
+	inf = b_animale.invelisul_corpului_arici()
+	if inf == 'tepi':
+		assert True
+	else:
+		assert False
+
