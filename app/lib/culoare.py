6971c5359bfdd1ccf323c35d9d9aff231d932e17
Nicu <busuiocn51@gmail.com>
2025-05-10 13:19:43 +0000

    Folderul lib

---
+++ app/lib/culoare.py
@@ -0,0 +1,2 @@
+def get_culoare():
+    return "Capibara are blana de culoare maro-roșiatică sau gri, ajutându-se de ea pentru camuflaj în natură."
---
+++ app/lib/descriere.py
@@ -0,0 +1,2 @@
+def get_descriere():
+    return "Capibara este cel mai mare rozător din lume. Trăiește în America de Sud și este un animal semiacvatic."
