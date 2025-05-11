eb905d01b4830d17784a53fcccd6913fa18511dd
Stefania Ionescu <stefalidia405@gmail.com>
2025-05-12 01:50:24 +0300

    Update

--- app/441D_animale.py
+++ app/441D_animale.py
@@ -1,19 +1,20 @@
 from flask import Flask
-from lib.biblioteca_animale import culoare_cal(), descriere_cal()
+from lib.biblioteca_animale import culoare_cal, descriere_cal
 
 app = Flask(__name__)
 
 @app.route('/cal')
-def cal();
+def cal():
   return "Aceasta este pagina dedicata calului."
 
 @app.route('/cal/culoare')
-def culoare_cal();
-   return culoare_cal()
+def culoare_cal():
+  return  "Are culori cum ar fi negru, maro, alb sau combinatii"
 
 @app.route('/cal/descriere')
-def descriere_cal();
-   return culoare_descriere()
+def descriere_cal():
+  return  "Este domestic"
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port = 5000)
