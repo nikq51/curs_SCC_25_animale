4ac3bfb6347931573b46084dd7692c9162d2206f
Nicu <busuiocn51@gmail.com>
2025-05-11 18:39:27 +0000

    Librarii

---
+++ app/lib/descriere.py
@@ -0,0 +1,2 @@
+def get_descriere(): 
+    return "Nevastuica este un mamifer mic, agil si carnivor, cunoscut pentru corpul sau alungit, picioarele scurte si blana maronie cu burtica alba."
---
+++ app/lib/habitat.py
@@ -0,0 +1,2 @@
+def get_habitat():
+    return "Se gasesc in paduri de foiase si conifere, pajisti si campii sau zone argricole" 
