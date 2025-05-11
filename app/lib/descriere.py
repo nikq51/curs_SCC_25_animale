8f24ce0de940393790de02ab37872492558aa187
mcatalin17 <mirea.catalin0217@gmail.com>
2025-05-11 22:34:30 +0300

    defined functions

---
+++ app/lib/descriere.py
@@ -0,0 +1,2 @@
+def get_descriere(): 
+    return "Ratonul este un mamifer nocturn inteligent și curios, cunoscut pentru blana sa gri cu mască neagră în jurul ochilor și pentru abilitățile sale de a deschide obiecte cu labele dextereoase"
---
+++ app/lib/habitat.py
@@ -0,0 +1,2 @@
+def get_habitat():
+    return "Ratonii trăiesc în păduri, zone umede, regiuni rurale și chiar în orașe."
