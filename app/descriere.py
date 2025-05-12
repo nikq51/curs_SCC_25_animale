b4264ff2362bdcc484427b69992b8795887d18a2
Claudiu123Manea <claudiumanea95@yahoo.com>
2025-05-12 14:56:44 +0300

    Create descriere.py

---
+++ app/descriere.py
@@ -0,0 +1,2 @@
+def get_descriere():
+    return "Bizonul este un mamifer erbivor masiv din familia bovinelor, cunoscut pentru corpul său robust, gâtul scurt, cocoașa musculară și blana deasă, în special în partea anterioară a corpului. Există două specii principale: bizonul american (Bison bison) și bizonul european (Bison bonasus, cunoscut și ca zimbru)."
