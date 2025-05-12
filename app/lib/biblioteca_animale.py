532ad3d02d7f3f4f0cbc6eb2560ed63d9da0942c
TeodorCuriman <43408138+TeodorCuriman@users.noreply.github.com>
2025-05-12 10:41:12 +0300

    Create biblioteca_animale.py

---
+++ app/lib/biblioteca_animale.py
@@ -0,0 +1,10 @@
+# app/lib/biblioteca_animale.py
+
+def culoare_lup():
+    return "Lupul are o blană care variază de la gri deschis, maro, roșcat până la negru, în funcție de regiune și subspecie."
+
+def descriere_lup():
+    return (
+        "Lupul este un mamifer carnivor, inteligent și social, care trăiește în haite. "
+        "Este cunoscut pentru forța, adaptabilitatea și modul său organizat de vânătoare."
+    )
