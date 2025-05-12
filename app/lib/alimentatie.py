d24fba39a959edc9ae628c22fef86bfb05fb5840
Chitoaca Andreea <andreea.chirtoaca03@gmail.com>
2025-05-12 12:55:30 +0000

    am actualizat gitignore

---
+++ app/lib/alimentatie.py
@@ -0,0 +1,2 @@
+def get_alimentatie():
+    return "Panda se hrănește în principal cu bambus."
---
+++ app/lib/culoare.py
@@ -0,0 +1,2 @@
+def get_culoare():
+    return "Panda are blana albă cu pete negre pe urechi, ochi și picioare."
