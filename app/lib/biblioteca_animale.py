cd3893351584996e5b0eced01203e78f5d780e22
Baranga Andreea <barangaa062@gmail.com>
2025-05-15 19:20:12 +0000

    Proiect

---
+++ app/lib/biblioteca_animale.py
@@ -0,0 +1,5 @@
+def culoare_zebra():
+    return "Zebrele au un tipar distinctiv de dungi negre și albe."
+
+def descriere_zebra():
+    return "Zebra este un animal erbivor care trăiește în savanele Africii. Este cunoscută pentru dungile sale unice."
